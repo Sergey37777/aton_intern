3010a2f4db017e488db95ca1d2dd237c652d96d7
sergejlapin <sergey.l37@icloud.com>
2024-06-05 19:05:34 +0300

    refactored main

--- main.py
+++ main.py
@@ -1,13 +1,43 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException, Request, Form
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+from auth.auth_router import router, get_username
+from database.models import Client, User, Base
+from database.engine import get_async_session, engine
+
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+app.include_router(router, prefix='/auth')
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.on_event("startup")
+async def startup_event():
+    async with engine.begin() as conn:
+        # Удалить все существующие таблицы
+        await conn.run_sync(Base.metadata.drop_all)
+        # Создать их заново
+        await conn.run_sync(Base.metadata.create_all)
+
+    print("База данных была пересоздана.")
+
+
+@app.get("/", response_class=HTMLResponse)
+async def root(request: Request, token=Depends(get_username)):
+    return templates.TemplateResponse(
+        request=request, name="base.html", context={'id': id, 'token': token}
+    )
+
+
+@app.get("/clients")
+async def say_hello(session: AsyncSession = Depends(get_async_session)):
+    stmt = select(Client)
+    result = await session.execute(stmt)
+    result = result.scalars().all()
+    return {"message": result}
