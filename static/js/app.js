00735119afa8fc236525f4db2364caacc09a3e96
sergejlapin <sergey.l37@icloud.com>
2024-06-17 18:27:20 +0300

    refactored app.js

--- static/js/app.js
+++ static/js/app.js
@@ -20,14 +20,24 @@ document.getElementById('login-btn').onclick = function() {
     document.getElementById('login-form').style.display = 'block';
 }
 
+document.getElementById('register-btn').onclick = function() {
+    document.getElementById('register-form').style.display = 'block';
+}
+
 document.getElementsByClassName('close')[0].onclick = function() {
     document.getElementById('login-form').style.display = 'none';
 }
 
+document.getElementsByClassName('close')[1].onclick = function() {
+        document.getElementById('register-form').style.display = 'none';
+}
+
 // Закрыть, если клик вне окна
 window.onclick = function(event) {
-    if (event.target == document.getElementById('login-form')) {
+    if (event.target === document.getElementById('login-form') ||
+        event.target === document.getElementById('register-form')) {
         document.getElementById('login-form').style.display = 'none';
+        document.getElementById('register-form').style.display = 'none';
     }
 }
 
@@ -56,6 +66,33 @@ document.querySelector('#login-form form').onsubmit = async function(e) {
     } else {
         const error = await response.json();
         console.error('Login failed:', error.detail);
+    }
+}
+
+document.querySelector('#register-form form').onsubmit = async function(e) {
+    e.preventDefault();
+    const form = e.target;
+    const formData = new FormData(form);
+    const plainFormData = Object.fromEntries(formData.entries());
+    const formBody = new URLSearchParams(plainFormData);
+    const response = await fetch('/auth/signup', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: formBody
+    });
+
+
+    if (response.ok) {
+        const data = await response.json();
+        console.log("Successful sign up", data);
+        console.log(data);
+        console.log(data.message); // Должно вывести "Login successful"
+        window.location.reload();
+    } else {
+        const error = await response.json();
+        console.error('Sign up failed:', error.detail);
     }
 }
 
